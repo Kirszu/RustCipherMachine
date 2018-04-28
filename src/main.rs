a503618dab3e43991ac367ce100aeabe2221bd88
Marcin Kirsz <marcinkirsz@wp.pl>
2018-04-28 21:10:15 +0200

    Impementation of Caesar Encrypt method

--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,74 @@ fn main() {
         .expect("Failed to read line");
     let guess_num: u32 = guess.trim().parse().expect("Not a number!");
 
-    println!("Do you want to encrypt or decrypt your word?");
+    match guess_num {
+        1 => {println!("You choose: Caesar Cipher");
+              caesar_main();
+        },
+        2 => println!("You choose: Affine Cipher"),
+        3 => println!("You choose: Vinegere Cipher"),
+        _  => println!("{} is not a valid option", guess_num),
+    };
+}
+
+fn caesar_main () {
+    println!("Do you want to Encryt or Decrypt your word?");
     println!("Press 1 for Encrypt or 2 for Decrypt");
     let mut encr = String::new();;
         io::stdin().read_line(&mut encr)
         .expect("Failed to read line");
     let guess_encr: u32 = encr.trim().parse().expect("Not a number!");
-    
-    match guess_num {
-        1 => {println!("You chose: Caesar Cipher");
-		match guess_encr {
-			1 => println!("You want to Encrypt"),
-			2 => println!("You want to Decrypt"),
-			_  => println!("{} is not a valid option", guess_encr)
-		};
-	     },
-        2 => {println!("You chose: Affine Cipher");
-		match guess_encr {
-			1 => println!("You want to Encrypt"),
-			2 => println!("You want to Decrypt"),
-			_  => println!("{} is not a valid option", guess_encr)
-		};
-	     },
-        3 => {println!("You chose: Vinegere Cipher");
-		match guess_encr {
-			1 => println!("You want to Encrypt"),
-			2 => println!("You want to Decrypt"),
-			_  => println!("{} is not a valid option", guess_encr)
-		};
-	     },
-         _  => println!("{} is not a valid option", guess_num),
+
+    match guess_encr {
+        1 => {println!("You want to Encrypt");
+             caesar_encrypt();
+        },
+        2 => println!("You want to Decrypt"),
+        _  => println!("{} is not a valid option", guess_encr)
     };
+}
+
+fn caesar_encrypt () {
+    println!("Type your password you want to encrypt");
+    let mut password = String::new();;
+        io::stdin().read_line(&mut password)
+        .expect("Failed to read line");
+
+    println!("Select your key (number between 1 and 26)");
+    let mut key_input = String::new();;
+        io::stdin().read_line(&mut key_input)
+        .expect("Failed to read line");
+    let key: u8 = key_input.trim().parse().expect("Not a number!");
+
+    let mut chars_u8: Vec<u8> = password.into_bytes();
+    let mut chars_u8 = chars_u8.into_iter().filter(|&i|i != 10 && i != 13).collect::<Vec<_>>();
+    println!("{:?}", chars_u8);
+
+    let mut chars_u8_shifted: Vec<u8> = Vec::new();
+
+    for mut byte in chars_u8 {
+        if byte == 32 {
+            byte = 32;
+    // If statement for small letters
+        }
+        else if byte >= 97 {
+            if (byte + key > 122) {
+                byte = byte - 26 + key;
+            } else {
+                byte += key;
+            }
+        }
+    // If statement for capital letters
+         else if byte < 97 {
+            if (byte + key > 90) {
+                byte = byte - 26 + key;
+            } else {
+                byte += key;
+            }
+        }
+        chars_u8_shifted.push(byte);
+    }
+   println!("{:?}", chars_u8_shifted);
+   let result = String::from_utf8_lossy(&chars_u8_shifted);
+   println!("Result: {}", result);
 }
